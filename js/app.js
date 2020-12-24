e7b9222dc17f18bc675f67d4450a7a95eec49950
philrey12 <phil.raquinel@gmail.com>
2020-12-24 22:01:48 +0800

    Add search function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 // add to-do
 const generateTemplate = todo => {
@@ -28,4 +29,20 @@ list.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
     }
+});
+
+// search: keyup event
+const filterTodos = term => {
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove('filtered'));
+};
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
